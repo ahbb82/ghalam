fbe50dc2c89487a2dd93c33d3273f52a2577723a
ahbb82 <ahbb82@gmail.com>
2025-04-17 05:19:12 -0700

    making method

---
+++ lib/Helper/api_service.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:univercity/Public/public.dart';
+import
+
+class ApiService {
+  static final Dio dio = Dio();
+
+  static get(String url)async {
+    var res = await dio.get(baseUrl + url);
+    return res;
+  }
+
+  static post(String url,Map? body) async {
+    var res = await dio.post(baseUrl + url,data: body);
+    return res;
+  }
+}
+
