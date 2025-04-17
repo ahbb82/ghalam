79318e2fecc1db9cce4deb26c4ab95f38d46c645
ahbb82 <ahbb82@gmail.com>
2025-04-17 09:44:45 -0700

    writer_controller.dart

--- lib/Controller/writer_controller.dart
+++ lib/Controller/writer_controller.dart
@@ -1,11 +1,17 @@
 import 'package:get/get.dart';
 import 'package:univercity/Helper/api_service.dart';
+import 'package:univercity/Helper/snackbar.dart';
 import 'package:univercity/Model/story.dart';
 import 'package:univercity/UI/Veiw/Writing/writed_list.dart';
+
+int writer_id = 0;
 
 class WriterController extends GetxController{
 
   static List<Story> writerStoriesList = [];
+
+  static String title = '';
+  static String text = '';
 
   static GetWriterStories(int num)async{
     var res = await ApiService.post('Writer/GetWriterStories',{
@@ -19,5 +25,15 @@ class WriterController extends GetxController{
     print(writerStoriesList);
     if(res.data['success']=true)
       Get.to(WritedList(true));
+    writer_id = num;
+  }
+
+  static AddStory()async{
+    var res = await ApiService.post('Writer/AddStory', {
+      "writerID": writer_id,
+      "title": title,
+      "text": text
+    });
+    showSnackbar(res.data['success']?snackTypes.success:snackTypes.error, res.data['message']);
   }
 }
